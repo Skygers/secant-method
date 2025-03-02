2a6ac904c65c2ca30e4230fe94fc2c95a299fd9b
Pandu Hafizh Ananta <114291289+Skygers@users.noreply.github.com>
2025-03-02 23:15:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def create_equation_section():
     st.latex(r"P_1 + \frac{1}{2}\rho v_1^2 + \rho gh_1 = P_2 + \frac{1}{2}\rho v_2^2 + \rho gh_2")
 
     st.markdown("""
-    Menyelesaikan Persamaan Bernoulli untuk mencari kecepatan v pada titik 2 ($v_2$).
+    Menyelesaikan Persamaan Bernoulli untuk mencari kecepatan fuida pada titik 2 ($v_2$).
     """)
 
 def validate_inputs(P1, P2, rho, g, h1, h2, v1):
@@ -72,7 +72,7 @@ def create_input_section():
         # Point 1 and 2 parameters
         col1, col2 = st.columns(2)
         with col1:
-            st.markdown("#### Parameters")
+            st.markdown("#### Parameter pada titik 1")
             P1 = st.number_input(
                 "Tekanan pada titik 1 (P₁) [Pa]",
                 value=101325.0,
@@ -94,7 +94,7 @@ def create_input_section():
             )
 
         with col2:
-            st.markdown("#### Parameters")
+            st.markdown("#### Parameter pada titik 2")
             P2 = st.number_input(
                 "Tekanan pada titik 2 (P₂) [Pa]",
                 value=101325.0,
@@ -112,12 +112,11 @@ def create_input_section():
     suggested_x0, suggested_x1 = suggest_initial_guesses(v1, P1, P2, rho, g, h1, h2)
 
     # Numerical method parameters in an expander
-    with st.expander("Numerical Method Parameters", expanded=False):
+    with st.expander("Parameter Metode Secant", expanded=False):
         st.markdown("""
         Metode secant memerlukan dua tebakan awal untuk v₂ [m/s]. Tebakan yang baik membantu nilai error konvergen lebih cepat..
 
-        **Saran berdasarkan fisika:
-
+        *** Saran berdasarkan fisika:
         -Berdasarkan hukum kekekalan energi, v₂ seharusnya mirip dengan v₁.
         -Untuk pipa yang menyempit, v₂ > v₁.
         -Untuk pipa yang melebar, v₂ < v₁.
